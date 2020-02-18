52b014762d326f2942d2c7dd8ce0681bd59b12d4
shalomdaniel <shalomdaniel9480@gmail.com>
2020-02-18 10:32:42 +0530

    code upload

---
+++ src/org/pack/Details.java
@@ -0,0 +1,12 @@
+package org.pack;
+
+public class Details {
+
+	public static void main(String[] args) {
+		
+		Details d=new Details();
+		
+		System.out.println("java class");
+		System.out.println("Greens");
+	}
+}
